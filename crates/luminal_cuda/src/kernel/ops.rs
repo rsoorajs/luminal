a945e03a1409e7bb6bac42e3fb44f33f0303daed
jonah <jsamost@gmail.com>
2026-01-05 15:47:00 -0800

    added mean reduce kernel

--- crates/luminal_cuda/src/kernel/ops.rs
+++ crates/luminal_cuda/src/kernel/ops.rs
@@ -18,7 +18,7 @@ use luminal::{
 
 pub type Ops = (
     KernelAdd, KernelMul, KernelIota, KernelGather,
-    KernelSumReduce, KernelMaxReduce,
+    KernelSumReduce, KernelMaxReduce, KernelMeanReduce,
 );
 
 #[derive(Default, Debug, Clone)]
@@ -173,6 +173,184 @@ extern \"C\" {{
         let ptx = compile_ptx(&kernel).unwrap();
         let module = ctx.load_module(ptx).unwrap();
         let func = module.load_function("reduce_max_k").unwrap();
+
+        let constants = vars
+            .into_iter()
+            .map(|d| (d, module.get_global(&format!("const_{d}"), stream).unwrap()))
+            .collect();
+
+        (
+            func,
+            module,
+            kernel,
+            (n_outputs, 1.into(), 1.into()),                // grid
+            (threads_per_block.into(), 1.into(), 1.into()), // blocks
+            32.into(),                                      // shmem size
+            constants,
+        )
+    }
+
+    fn output_size(&self) -> Expression {
+        self.out_shape.iter().copied().product()
+    }
+}
+
+
+#[derive(Default, Debug, Clone)]
+
+pub struct KernelMeanReduce {
+    out_shape: Vec<Expression>,
+    iters: Expression,
+    in_stride: Vec<Expression>,
+    iter_stride: Expression,
+    out_stride: Vec<Expression>,
+    dtype: DType,
+}
+impl EgglogOp for KernelMeanReduce {
+    fn term(&self) -> (String, Vec<OpParam>) {
+        (
+            "KernelMean".to_string(),
+            vec![EList, Expr, Input, EList, Expr, EList, Dty],
+        )
+    }
+
+    fn rewrites(&self) -> Vec<String> {
+        vec!["
+(rule
+    (
+        (= ?sum (Sum ?out_shape ?iters ?inp ?in_stride ?iter_stride ?sum_out_stride))
+        (= ?iota (Iota ?iters ?one))
+        (= ?cast (Cast ?iota (F32)))
+        (= ?recip (Recip ?r_shape ?cast ?r_in_strides ?r_out_strides))
+        (= ?result (Mul ?shape ?sum ?sum_strides ?recip ?recip_strides ?out_strides))
+        (= ?dty (dtype ?inp))
+    )
+    (
+        (union ?result (KernelMean ?out_shape ?iters ?inp ?in_stride ?iter_stride ?out_strides ?dty))
+    )
+    :name \"kernel mean reduce\"
+)
+".to_string()]
+    }
+
+    fn cleanup(&self) -> bool {
+        false
+    }
+
+    fn extract<'a>(
+        &self,
+        egraph: &'a SerializedEGraph,
+        children: &[&'a ENodeId],
+        list_cache: &mut FxHashMap<&'a ENodeId, Vec<Expression>>,
+        expr_cache: &mut FxHashMap<&'a ENodeId, Expression>,
+    ) -> (LLIROp, Vec<&'a ENodeId>) {
+        (
+            LLIROp::new::<dyn KernelOp>(Box::new(Self {
+                out_shape: extract_expr_list(egraph, children[0], list_cache, expr_cache).unwrap(),
+                iters: extract_expr(egraph, children[1], expr_cache).unwrap(),           // ← extract_expr
+                in_stride: extract_expr_list(egraph, children[3], list_cache, expr_cache).unwrap(),
+                iter_stride: extract_expr(egraph, children[4], expr_cache).unwrap(),     // ← extract_expr
+                out_stride: extract_expr_list(egraph, children[5], list_cache, expr_cache).unwrap(),
+                dtype: extract_dtype(egraph, children[6]),                                // ← extract_dtype
+            }) as Box<dyn KernelOp>),                                                     // ← cast added
+            vec![children[2]],
+        )
+    }
+
+}
+
+
+impl KernelOp for KernelMeanReduce {
+    fn compile(
+        &self,
+        ctx: &Arc<CudaContext>,
+        stream: &Arc<CudaStream>,
+    ) -> (
+        CudaFunction,
+        Arc<CudaModule>,
+        String,
+        (Expression, Expression, Expression),
+        (Expression, Expression, Expression),
+        Expression,
+        FxHashMap<char, CudaSlice<u8>>,
+    ) {
+        let vars = self
+            .out_shape
+            .iter()
+            .flat_map(|e| e.dyn_vars())
+            .chain(self.in_stride.iter().flat_map(|e| e.dyn_vars()))
+            .chain(self.out_stride.iter().flat_map(|e| e.dyn_vars()))
+            .chain(self.iters.dyn_vars())
+            .chain(self.iter_stride.dyn_vars())
+            .collect::<FxHashSet<_>>();
+
+        let dtype = cuda_dtype(self.dtype);
+        let n_outputs: Expression = self.out_shape.iter().copied().product();
+        let threads_per_block = 256; // 8 warps per block
+
+        let kernel = format!(
+            "
+#define WARP_SIZE 32
+#define THREADS_PER_BLOCK 256
+#define FULL_MASK 0xffffffff
+{constants}
+extern \"C\" {{
+    __global__ void reduce_mean_k({dtype} *out, const {dtype} *in) {{
+        __shared__ {dtype} warp_sums[THREADS_PER_BLOCK / WARP_SIZE];
+        int const_z = blockIdx.x;
+
+        int tid = threadIdx.x;
+        int lane_id = tid % WARP_SIZE;
+        int warp_id = tid / WARP_SIZE;
+
+        int in_start = {in_index};
+        int iters = {iters};
+        int iter_stride = {iter_stride};
+        
+        {dtype} sum = 0;
+        for (int i = tid; i < iters; i += THREADS_PER_BLOCK) {{
+            sum += in[in_start + i * iter_stride];
+        }}
+
+        #pragma unroll
+        for (int s = WARP_SIZE / 2; s > 0; s /= 2) {{
+            sum += __shfl_down_sync(FULL_MASK, sum, s);
+        }}
+        
+        if (lane_id == 0) {{
+            warp_sums[warp_id] = sum;
+        }}
+        __syncthreads();
+        
+        if (warp_id == 0) {{
+            int cnt = THREADS_PER_BLOCK / WARP_SIZE;
+            {dtype} block_sum = tid < cnt ? warp_sums[tid] : 0;
+            
+            #pragma unroll
+            for (int s = cnt / 2; s > 0; s /= 2) {{
+                block_sum += __shfl_down_sync(FULL_MASK, block_sum, s);
+            }}
+            
+            if (tid == 0) {{
+                out[{out_index}] = ({dtype})(block_sum / (float)iters);
+            }}
+        }}
+    }}
+}}",
+            constants = vars
+                .iter()
+                .map(|i| format!("__constant__ int const_{i}[1];"))
+                .join("\n"),
+            dtype = dtype,
+            in_index = flatten_mul_strides(&self.out_shape, &self.in_stride).to_kernel(),
+            out_index = flatten_mul_strides(&self.out_shape, &self.out_stride).to_kernel(),
+            iters = self.iters.to_kernel(),
+            iter_stride = self.iter_stride.to_kernel(),
+        );
+
+        let ptx = compile_ptx(&kernel).unwrap();
+        let module = ctx.load_module(ptx).unwrap();
+        let func = module.load_function("reduce_mean_k").unwrap();
 
         let constants = vars
             .into_iter()
