808ad3d4e8610e28f68e1ba3afab657eb01d9466
austin_glover <austin@luminal.com>
2026-01-08 23:41:35 +0000

    fmt

--- crates/luminal_cuda/src/host/mod.rs
+++ crates/luminal_cuda/src/host/mod.rs
@@ -1,21 +1,19 @@
-use std::{
-    fmt::Debug,
-    vec::Vec,
-    sync::Arc,
-};
+use std::{fmt::Debug, sync::Arc, vec::Vec};
 
+use crate::cudarc::driver::{CudaSlice, CudaStream};
 use luminal::prelude::*;
-use crate::cudarc::driver::{CudaSlice, CudaStream}; 
 mod host_matmul;
 
-
-pub type Ops = (
-    host_matmul::HostMatmul,
-);
+pub type Ops = (host_matmul::HostMatmul,);
 
 pub trait HostOp: Debug + as_any::AsAny + EgglogOp {
     /// With the convention, and this is a bad way to do it, that the first is the output buffer.
-    fn execute(&self, stream: &Arc<CudaStream>, node_inputs: &mut Vec<CudaSlice<u8>>, dyn_map: &FxHashMap<char, usize>) -> anyhow::Result<()>;
+    fn execute(
+        &self,
+        stream: &Arc<CudaStream>,
+        node_inputs: &mut Vec<CudaSlice<u8>>,
+        dyn_map: &FxHashMap<char, usize>,
+    ) -> anyhow::Result<()>;
 
     fn output_size(&self) -> Expression;
 }
