057e40b26cd705b44e1adef0b24169c4eb0c0d77
Austin Glover <austin_glover@berekely.edu>
2026-03-03 19:55:41 +0000

    adding cuda dtype mapping

--- crates/luminal_cuda/src/lib.rs
+++ crates/luminal_cuda/src/lib.rs
@@ -15,11 +15,24 @@ use luminal::dtype::DType;
 
 fn cuda_dtype(dtype: DType) -> &'static str {
     match dtype {
+        DType::F64 => "double",
         DType::F32 => "float",
         DType::F16 => "half",
         DType::Bf16 => "__nv_bfloat16",
+        DType::TF32 => "float", // TF32 uses float storage, tensor cores handle the format
         DType::Int => "int",
-        DType::Bool | DType::NvFp4 | DType::Mxfp4 => "unsigned char",
+        DType::I16 => "short",
+        DType::U16 => "unsigned short",
+        DType::I8 => "signed char",
+        DType::U8 => "unsigned char",
+        DType::Bool => "unsigned char",
+        DType::F8E4M3 => "__nv_fp8_e4m3",
+        DType::F8E5M2 => "__nv_fp8_e5m2",
+        DType::F8UE8M0 => "__nv_fp8_e8m0",
+        DType::F6E2M3 => "__nv_fp6_e2m3",
+        DType::F6E3M2 => "__nv_fp6_e3m2",
+        DType::F4E2M1 => "__nv_fp4_e2m1",
+        DType::I4 | DType::U4 => "unsigned char", // Sub-byte, packed storage
     }
 }
 
